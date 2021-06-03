8e8bf347646668450b0c9bd02049144bf00712da
2629925922 <73739868+2629925922@users.noreply.github.com>
2021-06-03 10:52:06 +0800

    Update email_active.py

--- 网站建设/email_active.py
+++ 网站建设/email_active.py
@@ -8,5 +8,5 @@ MAIL_SERVER = "smtp.qq.com"
 MAIL_PORT = "587"
 MAIL_USE_TLS = True
 MAIL_USERNAME = "2629925922@qq.com"
-MAIL_PASSWORD = "mhp2629925922."
-MAIL_DEFAULT_SENDER = "2629925922@qq.com"
+MAIL_PASSWORD = "*******************"
+MAIL_DEFAULT_SENDER = "***************"
