072b53c5831ed09066c26a0907223a40101d626c
2629925922 <73739868+2629925922@users.noreply.github.com>
2021-10-15 19:18:41 +0800

    Add files via upload

--- 自定义游戏/2048/2048.cpp
+++ 自定义游戏/2048/2048.cpp
@@ -1,84 +1,90 @@
-#include <stdio.h>
+ï»¿#include <stdio.h>
 #include <stdlib.h>
 #include <conio.h>
 #include <stdbool.h>
+#include <string.h>
+
 #define SIZE 4
 #define random(x) rand()%x
 
 
-int map[4][4]={0};
+int map[4][4] = { 0 };
 void print_map();
 void randvalue();
+bool judge();
 struct spares
 {
-	int nums[2];	
+	int nums[2];
 }spare[16];
 
 //void getcommand(char command);
 
 int main()
 {
-	bool res=true;
+	bool res = true;
 	char command;
-	while(res)
+	while (res)
 	{
 		print_map();
-		if(!kbhit())
+		if (!_kbhit())
 		{
-			command = getch();
+			command = _getch();
 		}
+		res = judge();
 	}
-	
-	return 0;	
-} 
+	printf("æ¸¸æˆç»“æŸ\n%d", spare[0].nums[0]);
+	return 0;
+}
 
 void print_map()
 {
-	printf("2048Ğ¡ÓÎÏ·\n");
-	printf("wÊÇÉÏ£¬sÊÇÏÂ£¬aÊÇ×ó£¬dÊÇÓÒ\n");
+	printf("2048å°æ¸¸æˆ\n");
+	printf("wæ˜¯ä¸Šï¼Œsæ˜¯ä¸‹ï¼Œaæ˜¯å·¦ï¼Œdæ˜¯å³\n");
 	randvalue();
-	for(int i=0;i<SIZE;i++)
+	for (int i = 0; i < SIZE; i++)
 	{
-		for(int j=0;j<SIZE;j++)
+		for (int j = 0; j < SIZE; j++)
 		{
-			printf("%5d",map[i][j]);
-			if(j==3) printf("\n");
+			printf("%5d", map[i][j]);
+			if (j == 3) printf("\n");
 		}
-	
 	}
 }
 
 void randvalue()
 {
-//	bool ii = true;
-	int x_new,y_new,jjj,iii=0;
-	int iii=0;
-//	while(ii)
-//	{	
-//		x_new = random(3),y_new =random(3);
-//		if(map[x_new][y_new] == 0)
-//		{
-//			map[x_new][y_new] = 2;
-//			ii=false;
-//		}
-//	}
-	for(int i=0;i<SIZE;i++)
+	//	bool ii = true;
+	int x_new, y_new, jjj;
+	int iii = 0;
+	//	while(ii)
+	//	{	
+	//		x_new = random(3),y_new =random(3);
+	//		if(map[x_new][y_new] == 0)
+	//		{
+	//			map[x_new][y_new] = 2;
+	//			ii=false;
+	//		}
+	//	}
+	for (int i = 0; i < SIZE; i++)
 	{
-		for(int j=0;j<SIZE;j++)
-		{	
-			if(map[i][j] == 0) spare[iii++] = {i,j};
+		for (int j = 0; j < SIZE; j++)
+		{
+			if (map[i][j] == 0) spare[iii++] = { i,j };
 		}
 	}
-	for(int i=0;i<16;i++)
+	for (int i = 0; i < 16; i++)
 	{
-		printf("%3d",spare[i].nums[0]);
-		printf("%3d",spare[i].nums[1]);
-		printf("\n");
+		printf("%4d%4d\n", spare[i].nums[0], spare[i].nums[1]);
 	}
 	jjj = random(iii);
 	x_new = spare[jjj].nums[0];
 	y_new = spare[jjj].nums[1];
 	map[x_new][y_new] = 2;
+	spare->nums = { 0 };
 }
 
-
+bool judge()
+{
+	if (map[spare[0].nums[0]][spare[0].nums[1]] != 0) return false;
+	return true;
+}
